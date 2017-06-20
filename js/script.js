c6d3b380dadf4368e2457f7f55351048f3142d1e
Proca Pavel <proka.pavel@gmail.com>
2017-06-20 18:37:20 +0300

    123

--- js/script.js
+++ js/script.js
@@ -13,16 +13,20 @@ function showMenu(iconID) {
 			document.getElementById('load-icon').src = 'png/icon1.png';
 			document.getElementById('load-name').innerHTML = 'First icon';
 			document.getElementById('load').href = 'png/icon1.png';
+			document.getElementsByClassName('load')[0].href = 'svg/icon1.svg';
+			document.getElementsByClassName('load')[1].href = 'png/icon1.png';
 			break;
 		case 2:
 			document.getElementById('load-icon').src = 'png/icon2.png';
 			document.getElementById('load-name').innerHTML = 'Second icon';
-			document.getElementById('load').href = 'png/icon2.png';
+			document.getElementsByClassName('load')[0].href = 'svg/icon2.svg';
+			document.getElementsByClassName('load')[1].href = 'png/icon2.png';
 			break;
 		case 3:
 			document.getElementById('load-icon').src = 'png/icon3.png';
 			document.getElementById('load-name').innerHTML = 'Third icon';
-			document.getElementById('load').href = 'png/icon3.png';
+			document.getElementsByClassName('load')[0].href = 'svg/icon3.svg';
+			document.getElementsByClassName('load')[1].href = 'png/icon3.png';
 			break;
 		default: return;
 	}
